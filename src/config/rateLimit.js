d3cb3cc90a43ae860de48665f493b5ae922985ef
Jirayut Watvisoot <jwatvisoot@gmail.com>
2026-05-08 00:34:48 +0700

    Change identify rate limit from 5 to 3

--- src/config/rateLimit.js
+++ src/config/rateLimit.js
@@ -1,7 +1,7 @@
 // src/config/rateLimit.js
 // Requests per minute per user for each route group.
 module.exports = {
-  IDENTIFY: 5,  // Gemini 1.5 Flash global cap is 15 RPM
+  IDENTIFY: 3,  // Gemini 1.5 Flash global cap is 15 RPM
   SEARCH:   20, // Spoonacular
   RECIPE:   20, // Spoonacular
 };
